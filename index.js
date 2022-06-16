a68eea9d17c0b03d5d3a81a0d6e5f03fbebaf59c
Jonas Alves <joonasbalves@gmail.com>
2022-06-15 23:29:51 -0300

    add footer render

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ export default function ActivityRings({
   strokeWidth = 10,
   radius = 60,
   activeStrokeColor = '#EDAD2B',
-  activeStrokeSecondaryColor= '#DF1B47',
+  activeStrokeSecondaryColor= null,
   porcent = 10,
   multiple = [],
   style = {},
+  renderMiddle: RenderMiddle = null,
+  renderFooter: RenderFooter = null,
 }) {
 
   const viewBox = useMemo(() => {
@@ -22,58 +24,77 @@ export default function ActivityRings({
 
   return (
     <View style={{
+      width: radius*2,
+      height: radius*2,
+      position: "relative",
+    }}>
+
+      <View style={{
         width: radius*2,
         height: radius*2,
         position: "relative",
       }}>
-      <View style={styles.container}>
-        <Text style={{
-          color: "red"
-        }}>Testando</Text>
-      </View>
-      <Svg
-        width={radius * 2}
-        height={radius * 2}
-        viewBox={`0 0 ${viewBox * 2} ${viewBox * 2}`}
-        style={{
-          transform: [{ rotateZ: '270deg' }]
-        }}
-      >
-        {multiple.length > 0 ? 
-          multiple.map((ring,index) => {
-            return (
-              <Rings
-                key={index}
-                unique={index}
-                porcent={ring.porcent}
-                strokeWidth={ring.strokeWidth}
-                radius={ring.radius}
-                activeStrokeSecondaryColor={ring.activeStrokeSecondaryColor}
-                activeStrokeColor={ring.activeStrokeColor}
-              />
-            )
-          })
-        : (
-          <Rings
-            porcent={porcent}
-            strokeWidth={strokeWidth}
-            radius={radius}
-            activeStrokeSecondaryColor={activeStrokeSecondaryColor}
-            activeStrokeColor={activeStrokeColor}
-          />
+        {RenderMiddle && (
+          <View style={styles.container}>
+            <RenderMiddle />
+          </View>
         )}
-      </Svg>
+        <Svg
+          width={radius * 2}
+          height={radius * 2}
+          viewBox={`0 0 ${viewBox * 2} ${viewBox * 2}`}
+          style={{
+            transform: [{ rotateZ: '270deg' }],
+            position: "absolute",
+          }}
+        >
+          {multiple.length > 0 ? 
+            multiple.map((ring,index) => {
+              return (
+                <Rings
+                  key={index}
+                  unique={index}
+                  porcent={ring.porcent}
+                  strokeWidth={ring.strokeWidth}
+                  radius={ring.radius}
+                  activeStrokeSecondaryColor={ring.activeStrokeSecondaryColor}
+                  activeStrokeColor={ring.activeStrokeColor}
+                />
+              )
+            })
+          : (
+            <Rings
+              porcent={porcent}
+              strokeWidth={strokeWidth}
+              radius={radius}
+              activeStrokeSecondaryColor={activeStrokeSecondaryColor}
+              activeStrokeColor={activeStrokeColor}
+            />
+          )}
+        </Svg>
+      </View>
+      {RenderFooter && (
+        <View style={styles.renderFooter}>
+          <RenderFooter />
+        </View>
+      )}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignContent: "center",
-    backgroundColor: "#eaeaea",
-    position: "absolute",
+    flex: 1,
+    justifyContent: 'center',
+    alignContent:'center',
+    borderRadius: 10000,
+    alignSelf:"center",
   },
+  renderFooter: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent:'center',
+    alignSelf:"center",
+    marginTop: 8,
+  }
 });
